b4f84b0940f4f7f6e37dd08befd15947988d9028
Marcin Krawczyk <marckraw@icloud.com>
2025-04-26 15:07:53 +0200

    fix: make sure it gets OTA updates

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    async function checkForUpdate() {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          await Updates.reloadAsync();
+        }
+      } catch (e) {
+        console.log("Error checking for updates:", e);
+      }
+    }
+
+    checkForUpdate();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>🚀 Updated Mobile Horizon v0.0.2 🚀</Text>
